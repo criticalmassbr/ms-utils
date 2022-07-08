31ac0f92a59d695a1575381335f8b662551be67d
Jhoy1992 <jhonatanascari@gmail.com>
2022-07-08 03:02:51 -0300

    feat: add span functions to trace package

--- tracer.go
+++ tracer.go
@@ -16,6 +16,12 @@ import (
 
 type ITracer interface {
 	SetGlobalTracer(serviceName string, exportAddress string, exportPort string) error
+	NewSpan(ctx context.Context, tracerName string, spanName string) (context.Context, trace.Span)
+	SpanFromContext(ctx context.Context) trace.Span
+	AddSpanTags(span trace.Span, tags map[string]string)
+	AddSpanEvents(span trace.Span, name string, events map[string]string)
+	AddSpanError(span trace.Span, err error)
+	FailSpan(span trace.Span, msg string)
 }
 
 type TracerConfig struct {
@@ -50,8 +56,8 @@ func (t TracerConfig) SetGlobalTracer(c *TracerConfig) error {
 
 // NewSpan returns a new span from the global tracer. Each resulting
 // span must be completed with `defer span.End()` right after the call.
-func (t TracerConfig) NewSpan(ctx context.Context, name string) (context.Context, trace.Span) {
-	return otel.Tracer("").Start(ctx, name)
+func (t TracerConfig) NewSpan(ctx context.Context, tracerName string, spanName string) (context.Context, trace.Span) {
+	return otel.Tracer(tracerName).Start(ctx, spanName)
 }
 
 // SpanFromContext returns the current span from a context. If you wish to avoid
@@ -59,14 +65,14 @@ func (t TracerConfig) NewSpan(ctx context.Context, name string) (context.Context
 // this function throughout the application. With such practise you will get
 // flatter span tree as opposed to deeper version. You can always mix and match
 // both functions.
-func SpanFromContext(ctx context.Context) trace.Span {
+func (t TracerConfig) SpanFromContext(ctx context.Context) trace.Span {
 	return trace.SpanFromContext(ctx)
 }
 
 // AddSpanTags adds a new tags to the span. It will appear under "Tags" section
 // of the selected span. Use this if you think the tag and its value could be
 // useful while debugging.
-func AddSpanTags(span trace.Span, tags map[string]string) {
+func (t TracerConfig) AddSpanTags(span trace.Span, tags map[string]string) {
 	list := make([]attribute.KeyValue, len(tags))
 
 	var i int
@@ -81,7 +87,7 @@ func AddSpanTags(span trace.Span, tags map[string]string) {
 // AddSpanEvents adds a new events to the span. It will appear under the "Logs"
 // section of the selected span. Use this if the event could mean anything
 // valuable while debugging.
-func AddSpanEvents(span trace.Span, name string, events map[string]string) {
+func (t TracerConfig) AddSpanEvents(span trace.Span, name string, events map[string]string) {
 	list := make([]trace.EventOption, len(events))
 
 	var i int
@@ -97,13 +103,13 @@ func AddSpanEvents(span trace.Span, name string, events map[string]string) {
 // section of the selected span. This is not going to flag the span as "failed".
 // Use this if you think you should log any exceptions such as critical, error,
 // warning, caution etc. Avoid logging sensitive data!
-func AddSpanError(span trace.Span, err error) {
+func (t TracerConfig) AddSpanError(span trace.Span, err error) {
 	span.RecordError(err)
 }
 
 // FailSpan flags the span as "failed" and adds "error" label on listed trace.
 // Use this after calling the `AddSpanError` function so that there is some sort
 // of relevant exception logged against it.
-func FailSpan(span trace.Span, msg string) {
+func (t TracerConfig) FailSpan(span trace.Span, msg string) {
 	span.SetStatus(codes.Error, msg)
 }
