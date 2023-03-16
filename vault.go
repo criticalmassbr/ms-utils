9f758e7b1714009f71ad547edc87662cf485176f
Anderson Hese <anderson.hese@gmail.com>
2023-03-16 11:07:26 -0300

    refactor: vault functions

--- vault.go
+++ vault.go
@@ -5,24 +5,12 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 
 	"github.com/hashicorp/vault/api"
 	auth "github.com/hashicorp/vault/api/auth/approle"
 )
-
-type IVaultService interface {
-	GetDBInstanceConfig(client string) (VaultDBInstanceConfig, error)
-}
-
-type VaultDBInstanceConfig struct {
-	Host string
-	Name string
-	Pass string
-	User string
-}
 
 type VaultConfig struct {
 	RoleId    string
@@ -35,52 +23,26 @@ type VaultConfig struct {
 type vaultService struct {
 	config *VaultConfig
 	client *api.Client
-	IVaultService
 }
 
 type VaultSecretKey string
 
-const (
-	VSK_DATABASE_HOST VaultSecretKey = "DATABASE_HOST"
-	VSK_DATABASE_NAME VaultSecretKey = "DATABASE_NAME"
-	VSK_DATABASE_PASS VaultSecretKey = "DATABASE_PASS"
-	VSK_DATABASE_USER VaultSecretKey = "DATABASE_USER"
-)
-
 var Vault = vaultService{}
 
-func (v *vaultService) NewVaultService(cfg *VaultConfig) IVaultService {
-	service := &vaultService{
+func (v *vaultService) NewVaultService(cfg *VaultConfig) vaultService {
+	service := vaultService{
 		config: cfg,
 	}
 	service.init()
 	return service
 }
 
-func (s *vaultService) GetDBInstanceConfig(clientSlug string) (VaultDBInstanceConfig, error) {
-	var config VaultDBInstanceConfig
-	err := make([]error, 4)
-
-	config.Host, err[0] = s.getSecret(VSK_DATABASE_HOST, clientSlug)
-	config.Name, err[1] = s.getSecret(VSK_DATABASE_NAME, clientSlug)
-	config.Pass, err[2] = s.getSecret(VSK_DATABASE_PASS, clientSlug)
-	config.User, err[3] = s.getSecret(VSK_DATABASE_USER, clientSlug)
-
-	for _, e := range err {
-		if e != nil {
-			return VaultDBInstanceConfig{}, e
-		}
-	}
-
-	return config, nil
-}
-
-func (s *vaultService) init() {
+func (s *vaultService) init() error {
 	certs := x509.NewCertPool()
 
 	pemData, err := os.ReadFile(s.config.Cert)
 	if err != nil {
-		log.Fatalf("unable to read Vault certificate: %v", err)
+		return fmt.Errorf("unable to read Vault certificate: %v", err)
 	}
 	certs.AppendCertsFromPEM(pemData)
 
@@ -97,15 +59,33 @@ func (s *vaultService) init() {
 
 	client, err := api.NewClient(vaultConfig)
 	if err != nil {
-		log.Fatalf("unable to initialize Vault client: %v", err)
+		return fmt.Errorf("unable to initialize Vault client: %v", err)
 	}
 	s.client = client
 
 	_, err = s.login()
 	if err != nil {
-		log.Fatalf("unable to login to Vault: %v", err)
+		return fmt.Errorf("unable to login to Vault: %v", err)
 	}
 	go s.renewToken()
+	return nil
+}
+
+func (s *vaultService) login() (*api.Secret, error) {
+	appRoleAuth, err := s.newAppRoleAuth()
+	if err != nil {
+		return nil, err
+	}
+
+	authInfo, err := s.client.Auth().Login(context.Background(), appRoleAuth)
+	if err != nil {
+		return nil, fmt.Errorf("unable to login to AppRole auth method: %w", err)
+	}
+	if authInfo == nil {
+		return nil, fmt.Errorf("no auth info was returned after login")
+	}
+
+	return authInfo, nil
 }
 
 func (s *vaultService) newAppRoleAuth() (*auth.AppRoleAuth, error) {
@@ -177,24 +157,7 @@ func (s *vaultService) manageTokenLifecycle(token *api.Secret) error {
 	}
 }
 
-func (s *vaultService) login() (*api.Secret, error) {
-	appRoleAuth, err := s.newAppRoleAuth()
-	if err != nil {
-		return nil, err
-	}
-
-	authInfo, err := s.client.Auth().Login(context.Background(), appRoleAuth)
-	if err != nil {
-		return nil, fmt.Errorf("unable to login to AppRole auth method: %w", err)
-	}
-	if authInfo == nil {
-		return nil, fmt.Errorf("no auth info was returned after login")
-	}
-
-	return authInfo, nil
-}
-
-func (s *vaultService) getSecret(key VaultSecretKey, clientSlug string) (string, error) {
+func (s *vaultService) GetSecret(key VaultSecretKey, clientSlug string) (string, error) {
 	secret, err := s.client.KVv1(s.config.MountPath).Get(context.Background(), clientSlug)
 	if err != nil {
 		return "", fmt.Errorf("unable to read secret: %v", err)
